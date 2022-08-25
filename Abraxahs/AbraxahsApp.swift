569ae6ccd6bf3212efb5424016047444f64872f9
Shervin Lepinat <shervin.lepinat@abraxas.ch>
2022-08-25 14:33:36 +0200

    Initial Commit

---
+++ Abraxahs/AbraxahsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbraxahsApp.swift
+//  Abraxahs
+//
+//  Created by Shervin Lepinat on 25.08.22.
+//
+
+import SwiftUI
+
+@main
+struct AbraxahsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
